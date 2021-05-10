b9d21487095164d6e71b9bc2239958294b572b0a
Cristofer Martinez <cristoferm@gmail.com>
2021-05-10 12:40:00 -0700

    javascript-operators-and-expressions | Cristofer Martinez

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,52 @@
+var width = 5;
+var height = 6;
+var area = width * height;
+
+console.log('Value of area:', area);
+console.log('typeof of area:', typeof area);
+
+// ii
+var bill = 10;
+var payment = 12;
+var change = payment - bill;
+
+console.log('Value of change:', change);
+console.log('typeof of change:', typeof change);
+
+// iii
+var quizzes = 90;
+var midterm = 80;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade:', grade);
+console.log('typeof of grade:', typeof grade);
+
+// iv
+var firstName = 'Cristofer';
+var lastName = 'Martinez';
+var fullName = firstName + ' ' + lastName;
+
+console.log('Value of fullName:', fullName);
+console.log('typeof of fullName:', typeof fullName);
+
+// v
+var pH = 8;
+var isAcidic = pH < 7;
+
+console.log('Value of isAcidic:', isAcidic);
+console.log('typeof of isAcidic:', typeof isAcidic);
+
+// vi
+var headCount = 11;
+var isSparta = headCount === 300;
+
+console.log('Value of isSparta:', isSparta);
+console.log('typeof of isSparta:', typeof isSparta);
+
+// vii
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+
+console.log('Value of motto:', motto);
+console.log('typeof of motto:', typeof motto);
