60f35b4a9774be02e285150c6565bac7cb53b375
Cristofer Martinez <cristoferm@gmail.com>
2021-05-17 23:20:18 -0700

    object-manipulation-2 | Cristofer Martinez

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,5 +1,22 @@
 /* exported pick */
 
-function pick(source, keys) {
+// store new object for desired properties and values
+// check each item in array
+// compare it to each porperty in the OG object
+// if it exists and if its not undefined
+//     store the property with it's value in the new object
+//     break subloop
+// return new object
 
+function pick(source, keys) {
+  var newOjb = {};
+  for (var i = 0; i < keys.length; i++) {
+    for (var prop in source) {
+      if (prop === keys[i] && source[prop] !== undefined) {
+        newOjb[keys[i]] = source[prop];
+        break;
+      }
+    }
+  }
+  return newOjb;
 }
