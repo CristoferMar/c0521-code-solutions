908ecce9206dd93788df100fcc83e653a69a4e31
Cristofer Martinez <cristoferm@gmail.com>
2021-05-20 15:02:29 -0700

    string-manipulation-3 | reverseWords in progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -3,3 +3,18 @@
 // this one is easy
 // you already did a reverse word. the only difference here is the split by ' '
 // then concat each reversed word
+
+// function reverseWords(string) {
+//   var arraycut = string.split(' ');
+//   var reversedWord = '';
+//   var reversedSentance = '';
+
+//   for (var i = 0; i < arraycut.length; i++) {
+//     for (var c = arraycut[i].length; c > 0; c--) {
+//       console.log(c);
+//     }
+//   }
+//   console.log(reversedWord);
+//   console.log(reversedSentance);
+
+// }
