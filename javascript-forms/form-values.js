a10b197f9ea3bb035501c926f5c5a77bc3ad17b9
Cristofer Martinez <cristoferm@gmail.com>
2021-05-19 17:24:13 -0700

    javascript-forms | Moved sideEffect var into function

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,11 +1,10 @@
 var $form = document.querySelector('#contact-form');
 
-var userInfo = {};
-
 $form.addEventListener('submit', submitHandle);
 
 function submitHandle(event) {
   event.preventDefault();
+  var userInfo = {};
   for (var i = 0; i < $form.length - 1; i++) {
     userInfo[$form[i].name] = $form[i].value;
   }
