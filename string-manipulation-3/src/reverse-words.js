059ecdecb6297743325ed14cd4b8ea597923749a
Cristofer Martinez <cristoferm@gmail.com>
2021-05-20 20:05:53 -0700

    string-manipulation-3 | titleCase

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -4,17 +4,26 @@
 // you already did a reverse word. the only difference here is the split by ' '
 // then concat each reversed word
 
-// function reverseWords(string) {
-//   var arraycut = string.split(' ');
-//   var reversedWord = '';
-//   var reversedSentance = '';
+// new storage for: split the sentance into items of an array
+// new storage for empty string, which will hold reversed reverseWords
 
-//   for (var i = 0; i < arraycut.length; i++) {
-//     for (var c = arraycut[i].length; c > 0; c--) {
-//       console.log(c);
-//     }
-//   }
-//   console.log(reversedWord);
-//   console.log(reversedSentance);
+// check each word of the split array
+// check each letter of the current word
+//       post it in decending order in reversewordS
+// concat reversedword into reversed sentance
+// followed by a space (' ')
+// at end of function, remove character at end, which will be an extra space
 
-// }
+function reverseWords(string) {
+  var arraycut = string.split(' ');
+  var reversedSentance = '';
+
+  for (var i = 0; i < arraycut.length; i++) {
+    for (var c = arraycut[i].length - 1; c >= 0; c--) {
+      reversedSentance += arraycut[i].charAt(c);
+    }
+    reversedSentance += ' ';
+  }
+  reversedSentance = reversedSentance.substr(0, reversedSentance.length - 1);
+  return reversedSentance;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,5 @@
 /* exported titleCase */
+
+function titleCase(title) {
+
+}
