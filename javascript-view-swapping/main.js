3b91e68e3976d60e68bcb634bffded4a344ce39e
Cristofer Martinez <cristoferm@gmail.com>
2021-05-20 17:26:10 -0700

    javascript-view-swapping | Cristofer Martinez

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -5,23 +5,25 @@ var $tabContainer = document.querySelector('.tab-container');
 $tabContainer.addEventListener('click', tabHandler);
 
 function tabHandler(event) {
-  if (event.target.matches('.tab')) {
-    for (var i = 0; i < $tabs.length; i++) {
-      if ($tabs[i] === event.target) {
-        $tabs[i].className = 'tab active';
-      } else {
-        $tabs[i].className = 'tab';
-      }
-    }
-
-    var dataView = event.target.getAttribute('data-view');
-
-    for (i = 0; i < $views.length; i++) {
-      if ($views[i].getAttribute('data-view') === dataView) {
-        $views[i].className = 'view';
-      } else {
-        $views[i].className = 'hidden';
-      }
+  if (!event.target.matches('.tab')) {
+    return;
+  }
+  for (var i = 0; i < $tabs.length; i++) {
+    if ($tabs[i] === event.target) {
+      $tabs[i].className = 'tab active';
+    } else {
+      $tabs[i].className = 'tab';
     }
   }
+
+  var dataView = event.target.getAttribute('data-view');
+
+  for (i = 0; i < $views.length; i++) {
+    if ($views[i].getAttribute('data-view') === dataView) {
+      $views[i].className = 'view';
+    } else {
+      $views[i].className = 'hidden';
+    }
+  }
+
 }
