92a82432f9aaa5f5de20950fb65b5c96c8c8d539
Cristofer Martinez <cristoferm@gmail.com>
2021-05-21 11:45:51 -0700

    javascript-and-json | Cristofer Martinez

---
+++ javascript-and-json/main.js
@@ -0,0 +1,32 @@
+var library = [
+  {
+    isbn: '123',
+    title: 'Amazing Ants',
+    author: 'Amy A'
+  },
+  {
+    isbn: '456',
+    title: 'Bewidering Beasts',
+    author: 'Billy B'
+  },
+  {
+    isbn: '789',
+    title: 'Challenging Cheetas',
+    author: 'Charly C'
+  }
+];
+
+console.log('Library:', library);
+console.log('Library Typeof:', typeof (library));
+
+var stingLibrary = JSON.stringify(library);
+console.log('stingLibrary:', stingLibrary);
+console.log('stingLibrary Typeof:', typeof (library));
+
+var student = JSON.stringify({ ID: '1185', name: 'Jimmy John' });
+console.log('student:', student);
+console.log('student Typeof:', typeof (student));
+
+var newStudent = JSON.parse(student);
+console.log('newStudent:', newStudent);
+console.log('newStudent Typeof:', typeof (newStudent));
