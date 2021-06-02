36864f37e30ddf2504410de52bb709da5fc011fe
Cristofer Martinez <cristoferm@gmail.com>
2021-06-02 11:44:08 -0700

    javascript-bank | Cristofer Martinez

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -32,7 +32,7 @@ Bank.prototype.getTotalAssets = function () {
   if (this.accounts.length === 0) { return 0; }
   var grandTotal = 0;
   for (var i = 0; i < this.accounts.length; i++) {
-    grandTotal += this.accounts[i].balance;
+    grandTotal += this.accounts[i].getBalance();
   }
   return grandTotal;
 };
