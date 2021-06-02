45e3da1ca1fdee8f82be8ab703ccc09facedd503
Cristofer Martinez <cristoferm@gmail.com>
2021-06-02 11:48:00 -0700

    javascript-timers | Updated to rely on starting variable

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,19 +1,12 @@
 var $h1 = document.querySelector('h1');
 
 function countDown() {
-  if (timerID === 3) {
-    $h1.textContent = 3;
-  }
-  if (timerID === 2) {
-    $h1.textContent = 2;
-  }
-  if (timerID === 1) {
-    $h1.textContent = 1;
-  }
   if (timerID === 0) {
     $h1.textContent = '~Earth Beeeelooowww Us~';
     clearInterval(intId);
+    return;
   }
+  $h1.textContent = timerID;
   timerID--;
 }
 
