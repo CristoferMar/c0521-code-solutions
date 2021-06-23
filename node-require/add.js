04120c2eeb985c5c0844fb0bd23c0b0b1fc72826
CristoferMar <cristoferm@gmail.com>
2021-06-23 10:52:25 -0700

    node-require

---
+++ node-require/add.js
@@ -0,0 +1,10 @@
+// let add = (x, y) => x + y;
+// exports.add = add;
+
+// export add = (x, y) => x + y;
+
+// module.exports = add = (x, y) => x + y;
+
+module.exports = function add(x, y) {
+  return x + y;
+};
