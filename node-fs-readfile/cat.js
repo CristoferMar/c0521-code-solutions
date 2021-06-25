d807738c7ec04da18a7309b296d66445f8e246fc
CristoferMar <cristoferm@gmail.com>
2021-06-25 10:04:34 -0700

    challenge 3 in progress

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,21 +1,26 @@
 const fs = require('fs');
 
 let i = 2;
-let fullData = '';
+let fullData = {};
 
-while (process.argv[i]) {
   const fileSelect = process.argv[i] => {
     fs.readFile(pathWay, 'utf-8', (err, data) => {
-      if (err) throw err;
-      fullData += data;
-
-      if (!process.argv[i]) {
-        console.log(fullData);
+      if (err) {
+        throw err;
+      } else if (process.argv[i]) {
+        fullData[i] = process.argv[i];
+      } else {
+        while (fullData[])
       }
+
+
     });
   };
 
+if (!process.argv[i]) {
+  console.log(fullData);
 }
+
 // we might need an 'ignition' function, which runs the item again, called from inside of the "load"
 
 fileSelect(process.argv[2]);
