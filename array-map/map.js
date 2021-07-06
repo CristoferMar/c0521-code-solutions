1ca94bab302675de2bc9240e1d02d0af1d67def2
CristoferMar <cristoferm@gmail.com>
2021-07-06 12:02:05 -0700

    challenge problem

--- array-map/map.js
+++ array-map/map.js
@@ -1,6 +1,8 @@
 function map(array, transform) {
   const modArray = [];
   for (let i = 0; i < array.length; i++) {
-    let modItem = 
+    const modItem = array[i];
+    modArray.push(transform(modItem));
   }
+  return modArray;
 }
