58e40bf0deb05a18b947ad0fb5adb5bb8a5677c1
CristoferMar <cristoferm@gmail.com>
2021-07-12 13:06:08 -0700

    removed extra app.get hendler

--- express-static/index.js
+++ express-static/index.js
@@ -8,11 +8,6 @@ console.log(absolutePath);
 
 const staticFile = app.use(express.static(absolutePath));
 
-app.get('/', (req, res) => {
-  res.status(200);
-  res.send(staticFile);
-});
-
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
